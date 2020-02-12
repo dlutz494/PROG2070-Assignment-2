8bec02c03a4478269e55f492ace55124c0ae2221
dlutz494 <dlutz494@gmail.com>
2020-02-12 18:54:30 -0500

    Program.cs and TriangleSolver.cs completed

--- DLAssign2/Program.cs
+++ DLAssign2/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Created By: David Lutz
+// Date Created: February 12, 2020
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +13,97 @@ namespace DLAssign2
     {
         static void Main(string[] args)
         {
+            // Variables
+            int option;
+
+            // Loop until the user exits the application
+            do
+            {
+                // Menu
+                do
+                {
+                    // Reset option variable
+                    option = 0;
+                    // Display the options
+                    Console.WriteLine("Please enter an option");
+                    Console.WriteLine("1. Enter Triangle Dimensions");
+                    Console.WriteLine("2. Exit");
+                    try
+                    {
+                        // Get the option from the user
+                        option = int.Parse(Console.ReadLine());
+                    }
+                    catch (Exception)
+                    {
+                        // Clear the screen and display an error
+                        Console.Clear();
+                        Console.WriteLine("Error, invalid input, please try again");
+                    }
+                } while (option == 0);
+
+                // Execute the chosen option
+                switch (option)
+                {
+                    // Enter triangle dimensions
+                    case 1:
+                        // Clear the console
+                        Console.Clear();
+                        // Get the sides of the triangle
+                        Console.WriteLine("Enter the length of the first side:");
+                        int len1 = GetLength();
+                        Console.Clear();
+                        Console.WriteLine("Enter the length of the second side:");
+                        int len2 = GetLength();
+                        Console.Clear();
+                        Console.WriteLine("Enter the length of the third side:");
+                        int len3 = GetLength();
+                        Console.Clear();
+                        // Analyze the lengths and display the results
+                        Console.WriteLine(TriangleSolver.Analyze(len1, len2, len3));
+                        break;
+                    // Exit the application
+                    case 2:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        break;
+                } 
+            } while (option != 2);
+        }
+        /// <summary>
+        /// Gets the length of a side of the triangle from the user
+        /// </summary>
+        /// <returns>The length from te user</returns>
+        private static int GetLength()
+        {
+            // Variables
+            int len = 0;
+
+            // Get a valid positive integer from the user
+            do
+            {
+                try
+                {
+                    // Get the length from the user
+                    len = int.Parse(Console.ReadLine());
+                    // Display an error if the input is a valid integer but 0 or less
+                    if (len <= 0)
+                    {
+                        // Display the error
+                        Console.WriteLine("Error, input must be a positive integer greater than 0, please try again");
+                        // Set the length to 0
+                        len = 0;
+                    }
+                }
+                catch (Exception)
+                {
+                    // Display the error
+                    Console.WriteLine("Error, invalid input, please try again");
+                }
+            } while (len == 0);
+
+            // Return the length
+            return len;
         }
     }
 }
---
+++ DLAssign2/TriangleSolver.cs
@@ -0,0 +1,73 @@
+﻿// Created By: David Lutz
+// Date Created: February 12, 2020
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLAssign2
+{
+    public static class TriangleSolver
+    {
+        /// <summary>
+        /// Analyze three length variables and return a string indicating if they create a valid triangle
+        /// </summary>
+        /// <param name="len1">Length of first side</param>
+        /// <param name="len2">Length of second side</param>
+        /// <param name="len3">Length of third side</param>
+        /// <returns>String containing the length variables and the triangle created by them, if valid</returns>
+        public static string Analyze(int len1, int len2, int len3)
+        {
+            // Determine if the triangle is valid
+            if (LengthCheck(len1, len2, len3) && LengthCheck(len1, len3, len2) && LengthCheck(len2, len3, len1))
+            {
+                // Determine the type of triangle
+                // Equilateral
+                if (len1 == len2 && len2 == len3)
+                {
+                    // Return the output
+                    return $"The Triangle ({len1}, {len2}, {len3}) is Equilateral";
+                }
+                // Isosceles
+                else if (len1 == len2 || len1 == len3 || len2 == len3)
+                {
+                    // Return the output
+                    return $"The Triangle ({len1}, {len2}, {len3}) is Isosceles";
+                }
+                // Scalene
+                else
+                {
+                    // Return the output
+                    return $"The Triangle ({len1}, {len2}, {len3}) is Scalene";
+                }
+            }
+            else
+            {
+                // Invalid triangle
+                return $"The Triangle ({len1}, {len2}, {len3}) is Invalid";
+            }
+        }
+
+        /// <summary>
+        /// Checks that the sum of two given lengths is greater than the third
+        /// </summary>
+        /// <param name="len1">First length</param>
+        /// <param name="len2">Second length</param>
+        /// <param name="len3">Third length</param>
+        /// <returns>True if the sum of the first two lengths is greater than the third length, false otherwise</returns>
+        public static bool LengthCheck(int len1, int len2, int len3)
+        {
+            // Return true if the sum of len1 and len2 is greater than len3
+            if (len1 + len2 > len3)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
